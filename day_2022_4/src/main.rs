39fdca796cff6634f1b2ee9106fc6c81ead6f160
Louis Boulanger <louis.boulanger74@hotmail.fr>
2023-11-25 13:36:30 +0100

    Better 2022 day 4 and 5

--- day_2022_4/src/main.rs
+++ day_2022_4/src/main.rs
@@ -1,6 +1,12 @@
-use aoc_utils::*;
+use std::ops::RangeInclusive;
 
-const INPUT: &str = include_str!("input.txt");
+use aoc_utils::*;
+use itertools::Itertools;
+
+// Uncomment the line below to include your problem input
+// const INPUT: &str = include_str!("input.txt");
+
+// The example given in the prompt.
 const EXAMPLE: &str = r#"2-4,6-8
 2-3,4-5
 5-7,7-9
@@ -10,51 +16,82 @@ const EXAMPLE: &str = r#"2-4,6-8
 "#;
 
 fn part_1(input: &str) {
-    let res = iter_input_raw(input)
-        .filter_map(|l| {
-            let parse =
-                |(a, b): (&str, &str)| (a.parse::<u32>().unwrap(), b.parse::<u32>().unwrap());
+    // In this part, we want to count how many of the couple of ranges
+    // have a complete overlap: either the first one completely
+    // overlaps the second, or vice-versa.
 
-            let (left, right) = l.split_once(',').unwrap();
-            let (left_start, left_end) = parse(left.split_once('-').unwrap());
-            let (right_start, right_end) = parse(right.split_once('-').unwrap());
+    // Let's iterate over each couple of ranges.
+    let result = input
+        .lines()
+        // We're gonna want to filter out all the couples that don't overlap.
+        .filter(|l| {
+            // This function parses a single range of the form
+            // "start-end", as they are defined in the input scheme.
+            let parse_range = |s: &str| {
+                let (start, end) = s.split_once('-').unwrap();
 
-            if (left_start >= right_start && left_end <= right_end)
-                || (right_start >= left_start && right_end <= left_end)
-            {
-                Some(())
-            } else {
-                None
-            }
+                start.parse::<u32>().unwrap()..=end.parse::<u32>().unwrap()
+            };
+
+            // This function parses a couple of ranges of the form
+            // "left,right", using the function above, as defined in
+            // the input scheme.
+            let parse_range_couple = |s: &str| {
+                let (left, right) = s.split_once(',').unwrap();
+
+                (parse_range(left), parse_range(right))
+            };
+
+            // This calculates whether the left range is completely included in the right range.
+            let overlaps = |left: &RangeInclusive<u32>, right: &RangeInclusive<u32>| {
+                left.start() >= right.start() && left.end() <= right.end()
+            };
+
+            // Parse our input into ranges.
+            let (left, right) = parse_range_couple(l);
+
+            // Since we want to check both cases (either left overlaps
+            // right, or right overlaps left), we need to check both
+            // cases.
+            overlaps(&left, &right) || overlaps(&right, &left)
         })
+        // The problem states we want to count the number of ranges that overlap.
         .count();
 
-    println!("{res}")
+    println!("Part 1: {result}");
 }
 
 fn part_2(input: &str) {
-    let res = iter_input_raw(input)
-        .filter_map(|l| {
-            let parse =
-                |(a, b): (&str, &str)| (a.parse::<u32>().unwrap(), b.parse::<u32>().unwrap());
+    // In this part, we now count the number of ranges that overlap
+    // even partially.
 
-            let (left, right) = l.split_once(',').unwrap();
-            let (left_start, left_end) = parse(left.split_once('-').unwrap());
-            let (right_start, right_end) = parse(right.split_once('-').unwrap());
+    let result = input
+        .lines()
+        .filter(|l| {
+            let parse_range = |s: &str| {
+                let (start, end) = s.split_once('-').unwrap();
 
-            if left_end < right_start || left_start > right_end {
-                None
-            } else {
-                Some(())
-            }
+                start.parse::<u32>().unwrap()..=end.parse::<u32>().unwrap()
+            };
+
+            let parse_range_couple = |s: &str| {
+                let (left, right) = s.split_once(',').unwrap();
+
+                (parse_range(left), parse_range(right))
+            };
+
+            let (left, right) = parse_range_couple(l);
+
+            !(left.end() < right.start() || left.start() > right.end())
         })
+        // The problem states we want to count the number of ranges that overlap.
         .count();
 
-    println!("{res}")
+    println!("Part 2: {result}");
 }
 
 fn main() {
-    let input = INPUT;
+    let input = EXAMPLE;
     part_1(input);
     part_2(input);
 }
--- day_2022_5/src/main.rs
+++ day_2022_5/src/main.rs
@@ -1,7 +1,10 @@
 use aoc_utils::*;
 use itertools::Itertools;
 
-const INPUT: &str = include_str!("input.txt");
+// Uncomment the line below to include your problem input
+// const INPUT: &str = include_str!("input.txt");
+
+// The example given in the prompt.
 const EXAMPLE: &str = r#"    [D]    
 [N] [C]    
 [Z] [M] [P]
@@ -13,102 +16,164 @@ move 2 from 2 to 1
 move 1 from 1 to 2
 "#;
 
-fn parse_stacks(input: &str) -> Vec<Vec<char>> {
-    let number_of_stacks = (input.lines().last().unwrap().chars().count() + 1) / 4;
+// We can model our stacks as a vector of stacks of characters; a
+// stack is also just a vector too.
+type Stacks = Vec<Vec<char>>;
 
-    let mut vecs = input
+// Let's parse the first half of the input to get the initial state of
+// the stacks.
+fn parse_stacks(input: &str) -> Stacks {
+    // First, let's get the number of stacks. We can easily compute it
+    // by realising that each stack will be represented by 4
+    // characters: eithe whitespace for nothing, or '[', a letter, ']'
+    // and ' '. Since the last stack is missing the last space, we add
+    // it manually.
+    let number_of_stacks = (input.lines().next().unwrap().chars().count() + 1) / 4;
+
+    // Let's iterate on each line of the stack.
+    input
         .lines()
-        .map(|l| {
-            format!("{l} ")
-                .chars()
+        // Go from the bottom, since we're working on bottom-to-top stacks.
+        .rev()
+        // Skip the first line here (so the last line of the input),
+        // which contains the labels of stacks, which we don't need.
+        .skip(1)
+        // And for each of these lines...
+        .map(|line| {
+            // Get an iterator on the characters of the line
+            line.chars()
+                // Add a space at the end
+                .chain(std::iter::once(' '))
+                // Divide the line in chunks of 4 characters
                 .chunks(4)
                 .into_iter()
-                .map(|mut c| {
-                    if c.next() == Some('[') {
-                        c.next()
+                // For each of those stack slices...
+                .map(|mut stack| {
+                    // If it's an element to put in the stack, get the
+                    // letter associated to it. Otherwise, return a
+                    // None.
+                    if let Some('[') = stack.next() {
+                        Some(stack.next().unwrap())
                     } else {
                         None
                     }
                 })
                 .collect::<Vec<_>>()
         })
+        // Now we construct the vector of stacks from the stack slices.
         .fold(
-            std::iter::repeat_with(Vec::new)
-                .take(number_of_stacks)
-                .collect::<Vec<_>>(),
-            |mut acc, level| {
-                for (v, c) in acc.iter_mut().zip(level.into_iter()) {
-                    if let Some(c) = c {
-                        v.push(c)
+            // At first, we have a vec of empty stacks.
+            vec![Vec::new(); number_of_stacks],
+            |mut acc, line| {
+                // For each potential element in each stack...
+                for (stack, elem) in acc.iter_mut().zip(line.into_iter()) {
+                    // If there is an element, add it to the stack.
+                    if let Some(elem) = elem {
+                        stack.push(elem);
                     }
                 }
                 acc
             },
-        );
-
-    for v in &mut vecs {
-        v.reverse()
-    }
-
-    vecs
-}
-
-fn parse_instructions(input: &str) -> impl Iterator<Item = (usize, usize, usize)> + '_ {
-    input.lines().map(|l| {
-        let l = l
-            .chars()
-            .map(|c| if !c.is_ascii_digit() { ' ' } else { c })
-            .collect::<String>();
-
-        let mut i = l.split_ascii_whitespace();
-        (
-            i.next().unwrap().parse().unwrap(),
-            i.next().unwrap().parse::<usize>().unwrap() - 1,
-            i.next().unwrap().parse::<usize>().unwrap() - 1,
         )
-    })
 }
 
 fn part_1(input: &str) {
-    let mut input = iter_input_newline_separated(input);
-    let stacks = input.next().unwrap();
-    let instructions = input.next().unwrap();
+    // The goal here is to parse the stacks given in the input and
+    // perform "crane-like" moves of elements between them, taking
+    // elements one at a time.
 
+    // Let's separate our input into two sections: the initial stacks
+    // state and the move instructions. They are separated by a double
+    // newline sequence in the input.
+    let (stacks, moves) = input.split_once("\n\n").unwrap();
+
+    // We parse our stacks based on the function defined above.
     let mut stacks = parse_stacks(stacks);
 
-    for (amount, src, dst) in parse_instructions(instructions) {
-        let idx = stacks[src].len() - amount;
-        let elems = stacks[src].drain(idx..).rev().collect::<Vec<_>>();
-        stacks[dst].extend(elems);
+    // For every move instruction...
+    for line in moves.lines() {
+        // We want to extract the amount, source and destination
+        // stacks from the line:
+        let [amount, source, dest] = line
+            // Split every word in the string
+            .split_ascii_whitespace()
+            // Keep only those that we can convert to numbers
+            .filter_map(|token| token.parse::<usize>().ok())
+            // Collect into a vector
+            .collect::<Vec<_>>()
+            // We can convert it into a sized array and unwrap because
+            // we know we're always going to get 3 numbers.
+            .try_into()
+            .unwrap();
+
+        // Now we're going to perform the move. First, get the index
+        // of the first element to be removed in the source stack.
+        let start_idx = stacks[source - 1].len() - amount;
+
+        // Then, extract those elements:
+        let crates = stacks[source - 1]
+            // Drain the vector starting at the calculated index up to the end...
+            .drain(start_idx..)
+            // Reverse the order since we are going to simulate picking up one element at a time...
+            .rev()
+            // Collect it into a temporary vector.
+            .collect::<Vec<_>>();
+
+        // Finally, extend the destination stack with the extracted
+        // elements.
+        stacks[dest - 1].extend(crates);
     }
 
-    for v in stacks {
-        print!("{}", v.last().unwrap());
+    print!("Part 1: ");
+    // Now that we have moved everything, we need to print the topmost
+    // elements of each stack. For each stack in our list of stacks...
+    for stack in stacks {
+        // Print the last element. We know there is always going to be
+        // at least one element in each stack, so unwrapping is
+        // fine. Note that we use print!() and not println!() so that
+        // we can print every character on the same line.
+        print!("{}", stack.last().unwrap())
     }
+    // Print a newline at the end to end the result string.
     println!();
 }
 
 fn part_2(input: &str) {
-    let mut input = iter_input_newline_separated(input);
-    let stacks = input.next().unwrap();
-    let instructions = input.next().unwrap();
+    // The part 2 is going to be identical to part 1, but we are only
+    // going to remove the reverse order on the extracted elements
+    // from a stack, since we can now pick up all the elements in one
+    // go.
 
+    let (stacks, moves) = input.split_once("\n\n").unwrap();
     let mut stacks = parse_stacks(stacks);
 
-    for (amount, src, dst) in parse_instructions(instructions) {
-        let idx = stacks[src].len() - amount;
-        let elems = stacks[src].drain(idx..).collect::<Vec<_>>();
-        stacks[dst].extend(elems);
+    for line in moves.lines() {
+        let [amount, source, dest] = line
+            .split_ascii_whitespace()
+            .filter_map(|token| token.parse::<usize>().ok())
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+
+        let start_idx = stacks[source - 1].len() - amount;
+
+        let crates = stacks[source - 1]
+            .drain(start_idx..)
+            // We remove the reverse order from here.
+            .collect::<Vec<_>>();
+
+        stacks[dest - 1].extend(crates);
     }
 
-    for v in stacks {
-        print!("{}", v.last().unwrap());
+    print!("Part 2: ");
+    for stack in stacks {
+        print!("{}", stack.last().unwrap())
     }
     println!();
 }
 
 fn main() {
-    let input = INPUT;
+    let input = EXAMPLE;
     part_1(input);
     part_2(input);
 }
