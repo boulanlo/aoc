2eb5d98b9280d23176b9fbd6e49b75d687496ad4
Louis Boulanger <louis.boulanger74@hotmail.fr>
2025-11-14 19:22:01 +0100

    Updated flake for 2025

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
     utils.url = "github:numtide/flake-utils/5aed5285a952e0b949eb3ba02c12fa4fcfef535f";
     rust-overlay.url = "github:oxalica/rust-overlay"; # use master since newer Rust versions are continuously pushed
   };
@@ -19,7 +19,7 @@
         };
 
         devShell = with pkgs; mkShell {
-          buildInputs = [ rust-bin.stable.latest.default rust-analyzer linuxKernel.packages.linux_6_1.perf ];
+          buildInputs = [ rust-bin.stable.latest.default rust-analyzer ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
         
